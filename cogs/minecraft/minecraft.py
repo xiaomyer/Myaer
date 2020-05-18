4607f55499df87b3beebd31fd5b787f479d94e22
MyerFire <myercheng0614@gmail.com>
2020-05-17 21:17:43 -0400

    idk probably stuff i forgot to add to the last commit

---
+++ cogs/minecraft/minecraft.py
@@ -0,0 +1,72 @@
+"""
+MIT License
+
+Copyright (c) 2020 MyerFire
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from discord.ext import commands
+import discord
+from core.discord.markdown import Markdown
+from core.minecraft.request import MojangAPI
+
+class Minecraft(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.markdown = Markdown()
+        self.mojang = MojangAPI()
+
+    @commands.command(name = "name")
+    @commands.max_concurrency(1, per = commands.BucketType.user)
+    async def uuid(self, ctx, player):
+        try:
+            player_data = await self.mojang.get_profile_name(player)
+            player_uuid_embed = discord.Embed(
+                name = "Player UUID",
+                description = f"{player_data['name']}\'s UUID is {player_data['uuid']}."
+            )
+            await ctx.send(embed = player_uuid_embed)
+        except NameError:
+            nameerror_embed = discord.Embed(
+                name = "Invalid input",
+                description = f"\"{player}\" is not a valid username."
+            )
+            await ctx.send(embed = nameerror_embed)
+
+    @commands.command(name = "uuid")
+    @commands.max_concurrency(1, per = commands.BucketType.user)
+    async def uuid(self, ctx, player):
+        try:
+            player_data = await self.mojang.get_profile_name(player)
+            player_uuid_embed = discord.Embed(
+                name = "Player UUID",
+                description = f"{player_data['name']}\'s UUID is {player_data['uuid']}."
+            )
+            await ctx.send(embed = player_uuid_embed)
+        except NameError:
+            nameerror_embed = discord.Embed(
+                name = "Invalid input",
+                description = f"\"{player}\" is not a valid username."
+            )
+            await ctx.send(embed = nameerror_embed)
+
+def setup(bot):
+    bot.add_cog(Minecraft(bot))
+    print("Reloaded cogs.minecraft.")
---
+++ core/minecraft/request.py
@@ -0,0 +1,67 @@
+"""
+MIT License
+
+Copyright (c) 2020 MyerFire
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+import aiohttp
+import json
+
+mojang_api = "https://api.mojang.com/"
+mojang_session_server = "https://sessionserver.mojang.com/"
+
+class MojangAPI():
+    async def get_profile(self, player): # When input could be name or UUID
+        try:
+            return await self.get_profile_name(player)
+        except NameError:
+            try:
+                return await self.get_profile_uuid(player)
+            except NameError:
+                raise NameError(f"Invalid player name or UUID {player}")
+
+    async def get_profile_name(self, player): # When input is name
+        try:
+            async with aiohttp.ClientSession() as session:
+                profile = await session.get(f"{mojang_api}users/profiles/minecraft/{player}")
+                profile_json = await profile.json()
+                profile_data = {
+                "name" : profile_json["name"], # Case sensitive display name
+                "uuid" : profile_json["id"]
+                }
+        except Exception: # Mojang API returns wrong mimetype if player does not exist
+            raise NameError(f"Player \"{player}\" does not exist")
+
+        return profile_data
+
+    async def get_profile_uuid(self, player): # When input is only UUID
+        try:
+            async with aiohttp.ClientSession() as session:
+                profile = await session.get(f"{mojang_session_server}session/minecraft/profile/{player.replace('-','')}") # Mojang session server does not accept UUIDs with "-"
+                profile_json = await profile.json()
+                profile_data = {
+                "name" : profile_json["name"],
+                "uuid" : profile_json["id"]
+                }
+        except Exception:
+            raise NameError(f"Invalid UUID \"{player}\"")
+
+        return profile_data
