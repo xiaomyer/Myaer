fc60e7868e79b181fb694d2681b5f2aaf16cb884
myerfire <myercheng0614@gmail.com>
2021-03-19 15:26:39 -0400

    guild exp history command

--- cogs/minecraft/hypixel/hypixel.py
+++ cogs/minecraft/hypixel/hypixel.py
@@ -24,7 +24,7 @@ SOFTWARE.
 
 import discord
 import humanfriendly
-from discord.ext import commands
+from discord.ext import commands, menus
 
 
 class Hypixel(commands.Cog):
@@ -69,10 +69,53 @@ class Hypixel(commands.Cog):
                 name="Guild",
                 value=f"{guild.display}\n"
                       f"Level: {guild.level.level}\n"
+                      f"Total EXP: {guild.experience}"
                       f"Members: {guild.members.count}",
                 inline=False
             )
         await ctx.reply(embed=embed)
+
+    @hypixel.group(invoke_without_command=True)
+    async def guild(self, ctx: commands.Context, query=None):
+        if not query:
+            player = await ctx.bot.hypixel.player.get(ctx=ctx, query=query)
+            if not player: return await ctx.reply(embed=ctx.bot.embed(ctx, f"You are not in a guild!"))
+            guild = await ctx.bot.hypixel.hypixel.guild.get(uuid=player.uuid)
+        else:
+            guild = await ctx.bot.hypixel.hypixel.guild.get(name=query)
+        embed = self._default_guild_embed(guild)
+        await ctx.reply(embed=embed)
+
+    @guild.command(aliases=["exp", "xp"])
+    async def experience(self, ctx: commands.Context, query=None):
+        await ctx.reply(embed=ctx.bot.static.embed(ctx, "This may take a while.."))
+        if not query:
+            player = await ctx.bot.hypixel.player.get(ctx=ctx, query=query)
+            if not player: return await ctx.reply(embed=ctx.bot.embed(ctx, f"You are not in a guild!"))
+            guild = await ctx.bot.hypixel.hypixel.guild.get(uuid=player.uuid)
+        else:
+            guild = await ctx.bot.hypixel.hypixel.guild.get(name=query)
+        members = [member for member in guild.members]
+        history = []
+        for member in members:
+            player = await member.get()
+            total = sum(member.exp_history.values())
+            history.append((player, total))
+        history.sort(key=lambda entry: entry[1], reverse=True)
+        string = [f"{player}: {experience:,d}" for player, experience in history]
+        await menus.MenuPages(source=ctx.bot.static.paginators.codeblock(string, ctx, discord.Embed(
+            color=guild.tag.color,
+            title=f"{guild.display} Weekly Guild XP",
+        )), clear_reactions_after=True).start(ctx)
+
+    @staticmethod
+    def _default_guild_embed(guild):
+        return discord.Embed(
+            color=guild.tag.color,
+            title=guild.display,
+            description=f"Level: {guild.level.level}\n"
+                        f"Members: {guild.members.count}"
+        )
 
 
 def setup(bot):
